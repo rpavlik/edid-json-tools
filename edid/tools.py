298e86f758be798e65a83fc87c7de7f2e0da4242
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-09-13 11:00:42 -0500

    Move BytesFromFile and printing of a hex table to a utility module

--- edid/tools.py
+++ edid/tools.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Defines tools for printing and formatting EDID information."""
+"""Defines tools for reading, printing, and formatting EDID information."""
 
 
 import collections
@@ -35,3 +35,32 @@ def ListTrueOnly(adict):
     A list of strings for which the boolean values were True in the dictionary.
   """
   return [x for x in adict if adict[x]]
+
+def BytesFromFile(filename):
+  """Reads the EDID from binary blob form into list form.
+
+  Args:
+    filename: The name of the binary blob.
+
+  Returns:
+    The list of bytes that make up the EDID.
+  """
+  with open(filename, 'rb') as f:
+    chunk = f.read()
+    return [int(x) for x in bytes(chunk)]
+
+def PrintHexData(data):
+  """Prints an array of values in hexadecimal form.
+
+  Args:
+    data: The data to be printed.
+  """
+  hex_rows = len(data) // 16
+
+  print('\t\t 0 1  2 3  4 5  6 7  8 9  A B  C D  E F')
+
+  for x in range(0, hex_rows):
+
+    start = 0x10 * x
+    row = '%02X%02X ' * 8 % tuple(data[start : start + 16])
+    print('0x%04X:\t\t%s' % (x, row))
